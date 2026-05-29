8f6bbaebb84c3aa8f810f132478078dbef122a90
Rodrigo Estrada <rodrigo@example.com>
2026-05-29 17:01:58 -0400

    fix(chessbox): ganancia OGG 5.0

--- js/games/chessbox.js
+++ js/games/chessbox.js
@@ -522,7 +522,7 @@ class ChessBoxGame {
       // Create gain node for volume/mute control
       if (!this.oggGain) {
         this.oggGain = audioCtx.createGain();
-        this.oggGain.gain.value = 3.0;
+        this.oggGain.gain.value = 5.0;
         this.oggGain.connect(audioCtx.destination);
       }
 
